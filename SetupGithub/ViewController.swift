c1d10ea44daf4f97291596dd3dcd598b1c1e9f6b
Bry <bryannahm1@gmail.com>
2022-08-02 11:58:56 -0700

    chicken

--- SetupGithub/ViewController.swift
+++ SetupGithub/ViewController.swift
@@ -9,10 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imagePic: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func goodLuck(_ sender: Any) {
+        imagePic.image = UIImage(named: "minion")
+    }
+    
 
 
 }
